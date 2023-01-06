a4156442e31827c313e2bf18436dad53e00115f3
margarethdis <margarethdani2021@gmail.com>
2023-01-06 00:49:21 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        
+    
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.Write($"Your name is{last_name}, {name}{last_name}.");
     }
 }
