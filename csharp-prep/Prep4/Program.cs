98949ccfbc881d0889b857de1f139125fb005f3b
margarethdis <margarethdani2021@gmail.com>
2023-01-13 12:49:28 -0600

    Number list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,21 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");
+        
+        Console.Write("Enter number: ");
+        int Number =  int.Parse(Console.ReadLine());
+        
+
+        List<int> numbers;
+        numbers = new List<int>();
+
+        numbers.Add(Number);
+        Console.WriteLine(numbers.Count);
+
+
     }
 }
